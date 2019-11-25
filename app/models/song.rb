a847153f73e57d4d49e898da30b85c7adb6b0b8b
Warren Yao <warrenyca905@gmail.com>
2019-11-25 04:56:12 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,10 +1,13 @@
 class Song < ApplicationRecord
+  include ActiveModel::Validations
+  
   validates :title, presence: true
   validates :artist_name, presence: true
   validates :released, inclusion: {in: [true, false]}
+  validates :title, uniqueness: { scope: [:release_year, :artist_name], message: "Cannot be repeated by the same artist in the same year." }
   
   with_options if: :released do |s|
     s.validates :release_year, presence: true
-    s.validates :release_year, 
-  
+    s.validates_with ReleaseYearValidator
+  end
 end
--- app/validators/release_year_validator.rb
+++ app/validators/release_year_validator.rb
@@ -1,6 +1,6 @@
 class ReleaseYearValidator < ActiveModel::Validator
   def validate(record)
-    unless record.release_year >= Time.now.year
+    unless !!record.release_year && record.release_year < Time.now.year
       record.errors[:release_year] << "Release year must be less than current year."
     end
   end
